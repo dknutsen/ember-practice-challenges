65ce1ce31be6c9bc8a4bb5816dd4614aba09f149
Dan Knutsen <dknutsen@gmail.com>
2022-10-10 18:18:44 -0400

    Add unit tests for user model

--- tests/unit/models/user-test.js
+++ tests/unit/models/user-test.js
@@ -1,13 +1,25 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'practice-challenges/tests/helpers';
 
+const EXPECTED = {
+  firstName: 'raw',
+  lastName:  'raw',
+  email:     'raw',
+  createdAt: 'date',
+  updatedAt: 'date',
+};
+
 module('Unit | Model | user', function (hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function (assert) {
+  test('it has the expected attributes', function (assert) {
     let store = this.owner.lookup('service:store');
     let model = store.createRecord('user', {});
-    assert.ok(model);
+    model.eachAttribute((_name, meta) => {
+      // unpack attribute information
+      const { type='raw', name } = meta;
+      assert.ok(Object.keys(EXPECTED).includes(name), `attribute ${name} found which was expected`);
+      assert.equal(EXPECTED[name], type, `attribute ${name} had expected type: ${EXPECTED[name]}`);
+    });
   });
 });
