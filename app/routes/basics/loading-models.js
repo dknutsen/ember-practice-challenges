4d3226a78918c6d61abce2593e138574207ec4f3
Dan Knutsen <dknutsen@gmail.com>
2022-10-12 16:48:00 -0700

    Add solution for basics.loading-models

--- app/routes/basics/loading-models.js
+++ app/routes/basics/loading-models.js
@@ -1,3 +1,10 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class BasicsLoadingModelsRoute extends Route {}
+export default class BasicsLoadingModelsRoute extends Route {
+  @service store;
+
+  model() {
+    return this.store.query('user', { page: 1, perPage: 5 })
+  }
+}
