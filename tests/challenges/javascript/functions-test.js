06bcf1a248d8f3cf6111ffae79e495fbf354caa7
Dan Knutsen <dknutsen@gmail.com>
2022-10-18 21:40:31 -0500

    Add one dumb javascript.functions challenge

---
+++ tests/challenges/javascript/functions-test.js
@@ -0,0 +1,17 @@
+import { module, test } from 'qunit';
+import { setupTest } from 'practice-challenges/tests/helpers';
+
+import {
+  TOP_SECRET_CODE,
+  classifiedProvider,
+} from 'practice-challenges/components/javascript/functions';
+
+module('Challenges | javascript.functions', function (hooks) {
+  setupTest(hooks);
+  module('classifiedProvider', function () {
+    test('returns the top secret variable', function (assert) {
+      const getTheValue = classifiedProvider();
+      assert.equal(getTheValue(), TOP_SECRET_CODE, 'the top secret code matches the top secret code');
+    });
+  });
+});
