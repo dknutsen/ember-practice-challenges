a7903a1232efdf789644166e4c9b7f3c1a4cbdb5
Dan Knutsen <dknutsen@gmail.com>
2022-12-15 15:08:21 -0600

    Comment out unfinished challenges for now

--- app/challenges.js
+++ app/challenges.js
@@ -115,6 +115,7 @@ export default {
       },
       'arbitrary-tracking': {
         title: 'Arbitrary Tracking',
+        notes: 'WORK IN PROGRESS - UNDER CONSTRUCTION',
         component: ArbitraryTracking,
       },
       memoization: {
@@ -128,12 +129,12 @@ export default {
   helpers: {
     title: 'Helpers',
     challenges: {
-      'function-helper': {
-        title: 'Function Helper',
-      },
-      'class-helper': {
-        title: 'Class Helper',
-      },
+      //'function-helper': {
+      //  title: 'Function Helper',
+      //},
+      //'class-helper': {
+      //  title: 'Class Helper',
+      //},
     },
   },
   modifiers: {
@@ -143,18 +144,18 @@ export default {
   routing: {
     title: 'Routing',
     challenges: {
-      'route-model': {
-        title: 'Forwarding Parent Model',
-        route: 'routing.route-model',
-      },
-      'nested-routes': {
-        title: 'Nested Routes',
-        notes:
-          'Nested routes are an important concept when working with the Ember Router. In this challenge we want to create a parent route with two child routes that show different data from the parent model',
-        route: 'routing.nested-routes',
-      },
-      transitions: { title: 'Transitions' }, // transitions via router service and redirects
-      'query-params': { title: 'Query Params' }, // add query params and transition with them
+      //'route-model': {
+      //  title: 'Forwarding Parent Model',
+      //  route: 'routing.parent-model',
+      //},
+      //'nested-routes': {
+      //  title: 'Nested Routes',
+      //  notes:
+      //    'Nested routes are an important concept when working with the Ember Router. In this challenge we want to create a parent route with two child routes that show different data from the parent model',
+      //  route: 'routing.nested-routes',
+      //},
+      //transitions: { title: 'Transitions' }, // transitions via router service and redirects
+      //'query-params': { title: 'Query Params' }, // add query params and transition with them
     },
   },
   'ember-data': {
@@ -164,9 +165,9 @@ export default {
   async: {
     title: 'Async',
     challenges: {
-      'async.actions': { title: 'Async Actions' },
-      'ember-concurrency': { title: 'Ember Concurrency' },
-      contagiousness: { title: 'Contagiousness' },
+      //'async.actions': { title: 'Async Actions' },
+      //'ember-concurrency': { title: 'Ember Concurrency' },
+      //contagiousness: { title: 'Contagiousness' },
     },
   },
   'contextual-components': {
@@ -176,34 +177,34 @@ export default {
   accessibility: {
     title: 'Accessibility',
     challenges: {
-      'form-control-labels': {
-        title: 'Form Control Labels',
-      },
-      'table-markup': {
-        title: 'Table Markup',
-      },
-      'color-contrast': {
-        title: 'Color Contrast',
-      },
-      'proper-headings': {
-        title: 'Proper Headings',
-      },
+      //'form-control-labels': {
+      //  title: 'Form Control Labels',
+      //},
+      //'table-markup': {
+      //  title: 'Table Markup',
+      //},
+      //'color-contrast': {
+      //  title: 'Color Contrast',
+      //},
+      //'proper-headings': {
+      //  title: 'Proper Headings',
+      //},
     },
   },
   refactoring: {
     title: 'Refactoring',
     challenges: {
-      'classic-to-glimmer': {
-        title: 'Classic to Glimmer',
-        notes:
-          'In this challenge we\'ll refactor a "classic" Ember Component to a Glimmer component. Note that the intent is to go from classic => glimmer but not necessarily al the way to strict-mode/SFC',
-        component: ClassicToGlimmer,
-      },
-      'glimmer-to-sfc': {
-        title: 'Glimmer to SFC',
-        notes:
-          "In this challenge we'll refactor a Glimmer Component to a strict-mode SFC component. Your solution should be in one *.gjs file and comply with strict mode rules.",
-      },
+      //'classic-to-glimmer': {
+      //  title: 'Classic to Glimmer',
+      //  notes:
+      //    'In this challenge we\'ll refactor a "classic" Ember Component to a Glimmer component. Note that the intent is to go from classic => glimmer but not necessarily al the way to strict-mode/SFC',
+      //  component: ClassicToGlimmer,
+      //},
+      //'glimmer-to-sfc': {
+      //  title: 'Glimmer to SFC',
+      //  notes:
+      //    "In this challenge we'll refactor a Glimmer Component to a strict-mode SFC component. Your solution should be in one *.gjs file and comply with strict mode rules.",
+      //},
     },
   },
 };
